ee3025e56c826d98b75ce6b577e10ccd4735e24c
Koichi Shiraishi <zchee.io@gmail.com>
2021-04-18 23:35:42 +0900

    doc: add doc.go for stub avo package

---
+++ doc.go
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: Copyright 2021 The avo-sandbox Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+package avod_sandbox
+
+// import avo packages for go.mod.
+import (
+	_ "github.com/mmcloughlin/avo/attr"
+	_ "github.com/mmcloughlin/avo/build"
+	_ "github.com/mmcloughlin/avo/buildtags"
+	_ "github.com/mmcloughlin/avo/gotypes"
+	_ "github.com/mmcloughlin/avo/ir"
+	_ "github.com/mmcloughlin/avo/operand"
+	_ "github.com/mmcloughlin/avo/pass"
+	_ "github.com/mmcloughlin/avo/printer"
+	_ "github.com/mmcloughlin/avo/reg"
+	_ "github.com/mmcloughlin/avo/src"
+	_ "github.com/mmcloughlin/avo/x86"
+)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zchee/avo-sandbox
 
 go 1.16
+
+require github.com/mmcloughlin/avo v0.1.1-0.20210416221212-4592e16ebb86
